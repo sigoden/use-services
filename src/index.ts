19a6f43806b6bc0fd314c10bfaa6ff0a33166b92
sigoden <sigoden@gmail.com>
2020-11-03 09:24:32 +0800

    feat: stop_key is string now, export eventNames

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { EventEmitter } from "events";
 
-export const NAME = "service";
-export const STOP_KEY = Symbol("stop");
+export let SERVICE_NAME = "service";
+export const STOP_KEY = "__stop__";
 
 export interface Config {
   ns: string;
   emitter?: EventEmitter;
+  srvName?: string;
 }
 
 export interface Ctor<S> {
@@ -16,51 +17,60 @@ export interface InitFn<A, S> {
   (config: Config, args: A, ctor?: Ctor<S>): Promise<S>;
 }
 
-export interface ServiceOption<A, S> {
+export interface ServiceOption<A, S, D> {
   init: InitFn<A, S>;
   args: A;
   ctor?: Ctor<S>;
+  deps?: D,
 }
 
-type ExtractService<Type> = Type extends ServiceOption<infer A, infer S> ? S : never;
+type ExtractService<Type> = Type extends ServiceOption<infer A, infer S, infer D> ? S : never;
 
 export type Services<T> = {[K in keyof T]: ExtractService<T[K]>};
 
 export async function initAllServices<
-  O extends {[k: string]: ServiceOption<any, any>}
+  O extends {[k: string]: ServiceOption<any, any, any>}
 >(config: Config, options: O): Promise<Services<O>> {
   const { emitter } = config;
-  emitter.emit(`${NAME}.init`);
+  emitter.emit(eventNames.init);
   const srvs = {};
   await Promise.all(Object.keys(options).map(async srvName => {
     try {
       const option = options[srvName];
-      const srv = await option.init(config, option.args, option.ctor);
-      emitter.emit(`${NAME}.init.${srvName}`, { srv });
+      const srv = await option.init({ ...config, srvName }, option.args, option.ctor);
+      emitter.emit(eventNames.srvInit(srvName), { srv });
       srvs[srvName] = srv;
     } catch (error) {
-      emitter.emit(`${NAME}.error`, { srvName, error });
+      emitter.emit(eventNames.error, { srvName, error });
       throw error;
     }
   }));
-  emitter.emit(`${NAME}.initAll`);
+  emitter.emit(`${SERVICE_NAME}.initAll`);
   return srvs as Services<O>;
 }
 
 export async function stopAllServices(config, srvs: any) {
   const { emitter } = config;
-  emitter.emit(`${NAME}.stop`);
+  emitter.emit(eventNames.stop);
   await Promise.all(Object.keys(srvs).map(async srvName => {
-    const srv = srvs[name];
+    const srv = srvs[srvName];
     try {
       if (srv[STOP_KEY]()) {
         await srv[STOP_KEY]();
       }
-      emitter.emit(`${NAME}.stop.${srvName}`);
+      emitter.emit(eventNames.srvStop(srvName));
     } catch (error) {
-      emitter.emit(`${NAME}.error`, { srvName, error });
+      emitter.emit(eventNames.error, { srvName, error });
       throw error;
     }
   }));
-  emitter.emit(`${NAME}.stopAll`);
+  emitter.emit(eventNames.stop);
+}
+
+export const eventNames = {
+  init: `${SERVICE_NAME}.init`,
+  stop: `${SERVICE_NAME}.stop`,
+  srvInit: srvName => `${SERVICE_NAME}.init.${srvName}`,
+  srvStop: srvName => `${SERVICE_NAME}.stop.${srvName}`,
+  error: `${SERVICE_NAME}.error`,
 }
