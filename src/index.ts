2b687eb32100eb2ac52c4a99ab6f13e0976a20e2
sigoden <sigoden@gmail.com>
2021-11-21 16:21:12 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from "events";
-import * as pEvent from "p-event";
+import { pEvent } from "p-event";
 import * as debug from "debug";
 
 export const SERVICE_NAME = "use-services";
@@ -17,7 +17,7 @@ export interface InitOption<A, S> {
 }
 
 export interface Ctor<S> {
-  new(...args): S;
+  new (...args): S;
 }
 
 export interface InitFn<A, S> {
@@ -31,67 +31,88 @@ export interface ServiceOption<A, S> {
   deps?: string[];
 }
 
-type ExtractService<Type> = Type extends ServiceOption<infer A, infer S> ? S : never;
+type ExtractService<Type> = Type extends ServiceOption<infer A, infer S>
+  ? S
+  : never;
 
-export type Services<T> = {[K in keyof T]: ExtractService<T[K]>};
+export type Services<T> = { [K in keyof T]: ExtractService<T[K]> };
 
 export async function useServices<
-  O extends {[k: string]: ServiceOption<any, any>}
->(srvs: Services<O>, app: string, emitter: EventEmitter, options: O): Promise<() => Promise<void>> {
+  O extends { [k: string]: ServiceOption<any, any> }
+>(
+  srvs: Services<O>,
+  app: string,
+  emitter: EventEmitter,
+  options: O
+): Promise<() => Promise<void>> {
   dbg("init");
   emitter.emit(eventNames.init);
   const srvDepents = {};
-  await Promise.all(Object.keys(options).map(async srvName => {
-    dbg("%s init", srvName);
-    try {
-      const { deps, args, init, ctor } = options[srvName];
-      const option: InitOption<any, any> = {
-        app,
-        emitter,
-        srvName,
-        deps: [],
-        ctor,
-        args,
-      };
-      option.deps = await Promise.all((deps || []).map(async depName => {
-        if (!options[depName]) throw new Error(`service ${depName} is depent by ${srvName}.deps.${depName} but missed`);
-        await pEvent(option.emitter, exports.eventNames.srvInit(depName));
-        if (!srvDepents[depName]) srvDepents[depName] = [];
-        srvDepents[depName].push(srvName);
-        return srvs[depName];
-      }));
-      const srv = await init(option);
-      (srvs as any)[srvName] = srv;
-      emitter.emit(eventNames.srvInit(srvName), { srv });
-      dbg("%s is ready", srvName);
-    } catch (error) {
-      dbg("%s is fail to init", srvName);
-      emitter.emit(eventNames.error, { srvName, error });
-      error.message = `${srvName}: ${error.message}`;
-      throw error;
-    }
-  }));
+  await Promise.all(
+    Object.keys(options).map(async (srvName) => {
+      dbg("%s init", srvName);
+      try {
+        const { deps, args, init, ctor } = options[srvName];
+        const option: InitOption<any, any> = {
+          app,
+          emitter,
+          srvName,
+          deps: [],
+          ctor,
+          args,
+        };
+        option.deps = await Promise.all(
+          (deps || []).map(async (depName) => {
+            if (!options[depName])
+              throw new Error(
+                `service ${depName} is depent by ${srvName}.deps.${depName} but missed`
+              );
+            await pEvent(option.emitter, exports.eventNames.srvInit(depName));
+            if (!srvDepents[depName]) srvDepents[depName] = [];
+            srvDepents[depName].push(srvName);
+            return srvs[depName];
+          })
+        );
+        const srv = await init(option);
+        (srvs as any)[srvName] = srv;
+        emitter.emit(eventNames.srvInit(srvName), { srv });
+        dbg("%s is ready", srvName);
+      } catch (error) {
+        dbg("%s is fail to init", srvName);
+        emitter.emit(eventNames.error, { srvName, error });
+        error.message = `${srvName}: ${error.message}`;
+        throw error;
+      }
+    })
+  );
   dbg("initAll");
   emitter.emit(eventNames.initAll);
   const stop = async () => {
     dbg("stop");
     emitter.emit(eventNames.stop);
-    await Promise.all(Object.keys(srvs).map(async srvName => {
-      dbg("%s is stopping", srvName);
-      const srv = srvs[srvName];
-      try {
-        if (srvDepents[srvName]) await Promise.all(srvDepents[srvName].map(depent => pEvent(emitter, eventNames.srvStop(depent))));
-        if (srv[STOP_KEY]) {
-          await srv[STOP_KEY]();
+    await Promise.all(
+      Object.keys(srvs).map(async (srvName) => {
+        dbg("%s is stopping", srvName);
+        const srv = srvs[srvName];
+        try {
+          if (srvDepents[srvName])
+            await Promise.all(
+              srvDepents[srvName].map((depent) =>
+                pEvent(emitter, eventNames.srvStop(depent))
+              )
+            );
+          if (srv[STOP_KEY]) {
+            await srv[STOP_KEY]();
+          }
+          emitter.emit(eventNames.srvStop(srvName));
+          dbg("%s is stopped", srvName);
+        } catch (error) {
+          dbg("%s is fail to stop", srvName);
+          emitter.emit(eventNames.error, { srvName, error });
+          throw error;
         }
-        emitter.emit(eventNames.srvStop(srvName));
-        dbg("%s is stopped", srvName);
-      } catch (error) {
-        dbg("%s is fail to stop", srvName);
-        emitter.emit(eventNames.error, { srvName, error });
-        throw error;
-      }
-    }));
+      })
+    );
     dbg("stopAll");
     emitter.emit(eventNames.stopAll);
   };
@@ -102,8 +123,8 @@ export const eventNames = {
   init: `${SERVICE_NAME}.init`,
   initAll: `${SERVICE_NAME}.initAll`,
   stop: `${SERVICE_NAME}.stop`,
-  srvInit: srvName => `${SERVICE_NAME}.init.${srvName}`,
-  srvStop: srvName => `${SERVICE_NAME}.stop.${srvName}`,
+  srvInit: (srvName) => `${SERVICE_NAME}.init.${srvName}`,
+  srvStop: (srvName) => `${SERVICE_NAME}.stop.${srvName}`,
   error: `${SERVICE_NAME}.error`,
   stopAll: `${SERVICE_NAME}.stopAll`,
 };
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,20 +1,25 @@
-import {  InitOption, ServiceOption, Services } from "../src";
-
+import useServices, { InitOption, ServiceOption, Services } from "../src";
+import { EventEmitter } from "events";
 
 export async function init<A>(options: InitOption<A, A>): Promise<A> {
   return options.args;
 }
 
-const settings =  {
+const settings = {
   k: "v",
-}
+};
 
 const options = {
   settings: {
     args: settings,
     init,
   } as ServiceOption<typeof settings, typeof settings>,
-}
+};
 
-let srvs: Services<typeof options>;
-srvs.settings.k
+const srvs: Services<typeof options> = {} as any;
+
+test("it works", async () => {
+  const emitter = new EventEmitter();
+  await useServices(srvs, "app", emitter, options);
+  expect(srvs.settings.k).toEqual("v");
+});
