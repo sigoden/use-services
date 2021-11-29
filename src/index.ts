7612a2fa99a5ede21dc8cda2eda2f73a3b7b7f6b
sigoden <sigoden@gmail.com>
2021-11-29 19:55:23 +0800

    update event name and add createInitFn

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,12 @@ export interface Ctor<S> {
   new (...args): S;
 }
 
-export interface InitFn<A, S> {
-  (option: InitOption<A, S>): Promise<S>;
+export type InitFn<A, S> = (option: InitOption<A, S>) => Promise<S>;
+
+export function createInitFn<A, S>(srvClass: Ctor<S>) {
+  return async (option: InitOption<A, S>) => {
+    return new (option.ctor || srvClass)(option) as S;
+  };
 }
 
 export interface ServiceOption<A, S> {
@@ -38,15 +42,14 @@ type ExtractService<Type> = Type extends ServiceOption<infer A, infer S>
 
 export type Services<T> = { [K in keyof T]: ExtractService<T[K]> };
 
-function useServices<O extends { [k: string]: ServiceOption<any, any> }>(
-  app: string,
-  options: O
-) {
+export default function useServices<
+  O extends { [k: string]: ServiceOption<any, any> }
+>(app: string, options: O) {
   const srvs: Services<O> = {} as any;
   const emitter = new EventEmitter();
   const init = async () => {
     dbg("init");
-    emitter.emit(eventNames.init);
+    emitter.emit(SERVICES_EVENTS.INIT_START);
     const srvDepents = {};
     await Promise.all(
       Object.keys(options).map(async (srvName) => {
@@ -74,22 +77,23 @@ function useServices<O extends { [k: string]: ServiceOption<any, any> }>(
             })
           );
           const srv = await init(option);
+          if (srv[INIT_KEY]) await srv[INIT_KEY]();
           (srvs as any)[srvName] = srv;
-          emitter.emit(eventNames.srvInit(srvName), { srv });
+          emitter.emit(SERVICES_EVENTS.SRV_INIT(srvName), { srv });
           dbg("%s is ready", srvName);
         } catch (error) {
           dbg("%s is fail to init", srvName);
-          emitter.emit(eventNames.error, { srvName, error });
+          emitter.emit(SERVICES_EVENTS.ERROR, { srvName, error });
           error.message = `${srvName}: ${error.message}`;
           throw error;
         }
       })
     );
     dbg("initAll");
-    emitter.emit(eventNames.initAll);
+    emitter.emit(SERVICES_EVENTS.INIT_END);
     return async () => {
       dbg("stop");
-      emitter.emit(eventNames.stop);
+      emitter.emit(SERVICES_EVENTS.STOP_START);
       await Promise.all(
         Object.keys(srvs).map(async (srvName) => {
           dbg("%s is stopping", srvName);
@@ -98,36 +102,34 @@ function useServices<O extends { [k: string]: ServiceOption<any, any> }>(
             if (srvDepents[srvName])
               await Promise.all(
                 srvDepents[srvName].map((depent) =>
-                  pEvent(emitter, eventNames.srvStop(depent))
+                  pEvent(emitter, SERVICES_EVENTS.SRV_STOP(depent))
                 )
               );
             if (srv[STOP_KEY]) {
               await srv[STOP_KEY]();
             }
-            emitter.emit(eventNames.srvStop(srvName));
+            emitter.emit(SERVICES_EVENTS.SRV_STOP(srvName));
             dbg("%s is stopped", srvName);
           } catch (error) {
             dbg("%s is fail to stop", srvName);
-            emitter.emit(eventNames.error, { srvName, error });
+            emitter.emit(SERVICES_EVENTS.ERROR, { srvName, error });
             throw error;
           }
         })
       );
       dbg("stopAll");
-      emitter.emit(eventNames.stopAll);
+      emitter.emit(SERVICES_EVENTS.STOP_END);
     };
   };
   return { srvs, init, emitter };
 }
 
-export const eventNames = {
-  init: `${SERVICE_NAME}.init`,
-  initAll: `${SERVICE_NAME}.initAll`,
-  stop: `${SERVICE_NAME}.stop`,
-  stopAll: `${SERVICE_NAME}.stopAll`,
-  srvInit: (srvName) => `${SERVICE_NAME}.init.${srvName}`,
-  srvStop: (srvName) => `${SERVICE_NAME}.stop.${srvName}`,
-  error: `${SERVICE_NAME}.error`,
+export const SERVICES_EVENTS = {
+  INIT_START: `${SERVICE_NAME}.init.start`,
+  INIT_END: `${SERVICE_NAME}.init.end`,
+  STOP_START: `${SERVICE_NAME}.stop.start`,
+  STOP_END: `${SERVICE_NAME}.stop.end`,
+  SRV_INIT: (srvName) => `${SERVICE_NAME}.init.${srvName}`,
+  SRV_STOP: (srvName) => `${SERVICE_NAME}.stop.${srvName}`,
+  ERROR: `${SERVICE_NAME}.error`,
 };
-
-export default useServices;
